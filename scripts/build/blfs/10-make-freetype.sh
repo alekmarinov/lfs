4945957612e0c5c4b6aa667394bc94376cc0a209
alekmarinov <alekmarinov@gmail.com>
2022-10-12 18:43:02 -0400

    fix freetype script

--- scripts/build/blfs/10-make-freetype.sh
+++ scripts/build/blfs/10-make-freetype.sh
@@ -20,7 +20,7 @@ tar -xf /sources/freetype-* -C /tmp/ \
     fi \
     && sed -ri "s:.*(AUX_MODULES.*valid):\1:" modules.cfg \
     && sed -r "s:.*(#.*SUBPIXEL_RENDERING) .*:\1:" \
-        -i include/freetype/config/ftoption.h; \
+        -i include/freetype/config/ftoption.h \
     && ./configure --prefix=/usr --enable-freetype-config --disable-static \
     && make \
     && make install \
