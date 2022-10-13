d831a922b7788d71f8c32f7ac841adec0e6447cb
alekmarinov <alekmarinov@gmail.com>
2022-10-13 02:24:43 -0400

    Fixing file path

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,7 +6,7 @@ echo "Start..."
 $LFS/scripts/tools/build-tools.sh
 
 # build rootfs
-$LFS/scripts/build/build-rootfs.sh
+$LFS/scripts/rootfs/build-rootfs.sh
 
 # build iso image
 $LFS/scripts/image/build-image.sh
