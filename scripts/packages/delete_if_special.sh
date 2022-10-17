5c32222e929a8ea28dc4b4d59e2e406f62958302
Alexander Marinov <alekmarinov@gmail.com>
2022-10-17 16:57:28 -0400

    Keep the special file in the package, we will use it at the time of install. Deleting only the corresponding file/dir in base to make sure it will be the best source of truth of the state after all installed packages

--- scripts/packages/delete_if_special.sh
+++ scripts/packages/delete_if_special.sh
@@ -6,6 +6,6 @@
 # This script is a helper of build.sh.
 
 while [[ $# -gt 0 ]]; do
-    [[ "$(file -ib $1)" = "inode/chardevice;"* ]] && rm -rf "$1" "$LFS_BASE/$1"
+    [[ "$(file -ib $1)" = "inode/chardevice;"* ]] && rm -rf "$LFS_BASE/$1"
     shift
 done
