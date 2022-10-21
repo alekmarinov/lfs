10935c82b0406182eea0a08190aabb5cc4db18a0
alekmarinov <alekmarinov@gmail.com>
2022-10-21 01:12:38 -0400

    Fixing tools

--- scripts/build.sh
+++ scripts/build.sh
@@ -46,18 +46,18 @@ flag_file="tmp/${script_name%.*}.ready"
 # log file on the chroot system
 log_file="/tmp/${script_name%.*}.log"
 package_name="tmp/${script_name%.*}.tar.gz"
-if [[ ! -f "$flag_file" || $o_force -eq 1 ]]; then
+if [ $o_tool -eq 1 ]; then
     echo -ne "...... $script_path -> $log_file"
-    if [ $o_tool -eq 1 ]; then
-        if [ ! -f "$script_path" ]; then
-            echo -ne "\r\nbuild.sh: Can't find script $script_path"
-            exit 1
-        fi
-        # If building tool need to add $LFS_BASE/tools/bin to PATH
-        export PATH+=:$LFS_BASE/tools/bin
-        "$script_path" > "$log_file" 2>&1
-        status=$?
-    else
+    if [ ! -f "$script_path" ]; then
+        echo -ne "\r\nbuild.sh: Can't find script $script_path"
+        exit 1
+    fi
+    # If building tool need to add $LFS_BASE/tools/bin to PATH
+    export PATH+=:$LFS_BASE/tools/bin
+    "$script_path" > "$log_file" 2>&1
+    status=$?
+else
+    if [[ ! -f "$flag_file" || $o_force -eq 1 ]]; then
         if [ ! -f "$LFS/$script_path" ]; then
             echo -ne "\r\nbuild.sh: Can't find script $LFS/$script_path"
             exit 1
@@ -76,29 +76,34 @@ if [[ ! -f "$flag_file" || $o_force -eq 1 ]]; then
             /bin/bash --login +h -c "sh -c '$script_path > $log_file 2>&1'"
         status=$?
         $LFS/scripts/packages/11-unmount-vkfs.sh > /dev/null 2>&1
+    else
+        echo "skipped $script_path"
+        exit 0
     fi
-    if [ $status -eq 0 ]; then
-        echo -ne "\rpassed"; echo
-        touch "$flag_file"
+fi
+if [ $status -eq 0 ]; then
+    echo -ne "\rpassed"; echo
 
+    if [ $o_tool -ne 1 ]; then
         if [ $o_package -eq 1 ]; then
             # Archive package
             tar cfz "$package_name" -C "$LFS_PACKAGE" .
         fi
-        if [ $o_tool -ne 1 ]; then
-            # Deleting special files/dirs from destination, copy the others
-            "$SCRIPT_DIR/packages/copy-or-del.sh" "$LFS_PACKAGE" "$LFS_BASE"
-            # Remove all from LFS_PACKAGE folder
-            rm -rf "$LFS_PACKAGE"/*
-        fi
-    else
-        echo -ne "\rfailed"; echo
-        # The log_file should remain in $LFS_PACKAGE/tmp
-        tail "$LFS_PACKAGE$log_file"
-        echo
-        # Exit with failure
-        exit 1
+        touch "$flag_file"
+        # Deleting special files/dirs from destination, copy the others
+        "$SCRIPT_DIR/packages/copy-or-del.sh" "$LFS_PACKAGE" "$LFS_BASE"
+        # Remove all from LFS_PACKAGE folder
+        rm -rf "$LFS_PACKAGE"/*
     fi
 else
-    echo "skipped $script_path"
+    echo -ne "\rfailed"; echo
+    # The log_file should remain in $LFS_PACKAGE/tmp
+    if [ $o_tool -eq 1 ]; then
+        tail "$LFS_PACKAGE$log_file"
+    else
+        tail "$log_file"
+    fi
+    echo
+    # Exit with failure
+    exit 1
 fi
