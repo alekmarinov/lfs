e9ace0a52d217d1518e92169a7eeb5576db86dd1
Alexander Marinov <alekmarinov@gmail.com>
2022-10-20 23:47:33 -0400

    Fix with link copy in copy-or-del.sh

--- scripts/packages/copy-or-del.sh
+++ scripts/packages/copy-or-del.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 # Loop a source directory and delete entry from destination if source file 
 # is special, copy to destination otherwise
+set -e
 
 SRC_DIR="$1"
 DST_DIR="$2"
 
 find -P "$SRC_DIR" | while read -r src; do
-    # remove $SRC_DIR from beginning of $srcfile
+    # remove $SRC_DIR from beginning of $src
     dst=${src#"$SRC_DIR"}
     # prefix with $DST_DIR
     dst=$(echo "$DST_DIR/$dst" | sed s#//*#/#g)
     if [[ "$(file -ib $src)" = "inode/chardevice;"* ]]; then
         # remove $dst file or directory if $src is special
-        rm -rf "$dst"
+        rm -rfv "$dst"
     else
         # if $src is a dir, create a dir $dst
-        if [ -d "$src" ]; then
-            mkdir -p "$dst"
-        elif [ -f "$src" ] || [ -L "$src" ]; then
+        if [ -f "$src" ] || [ -L "$src" ]; then
             # if $src is a file, copy it to $dst
-            cp -fP "$src" "$dst"
+            cp -fPv "$src" "$dst"
+        elif [ -d "$src" ]; then
+            mkdir -pv "$dst"
         else
             echo "Unexpected file type $(file -ib $src) of $src"
             exit 1
