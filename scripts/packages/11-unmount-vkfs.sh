b273bfff039b81dcd69e7fb0bef24079cf0757bd
Alexander Marinov <alekmarinov@gmail.com>
2022-10-24 02:06:39 -0400

    mount vkfs in lfs instead of base

--- scripts/packages/11-unmount-vkfs.sh
+++ scripts/packages/11-unmount-vkfs.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -e
 echo "Unmounting Virtual Kernel File Systems.."
+__NAME__=$(basename "$0")
+
+if [ "$LFS" == "" ]; then
+    echo "$__NAME__: $LFS is not defined"
+    exit 1
+fi
 
 # unmount VFS
-umount -v $LFS_BASE/dev/pts
-umount -v $LFS_BASE/dev
-umount -v $LFS_BASE/run
-umount -v $LFS_BASE/proc
-umount -v $LFS_BASE/sys
+umount -v $LFS/dev/pts
+umount -v $LFS/dev
+umount -v $LFS/run
+umount -v $LFS/proc
+umount -v $LFS/sys
--- scripts/packages/7.3-mount-vkfs.sh
+++ scripts/packages/7.3-mount-vkfs.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 echo "Mounting Virtual Kernel File Systems.."
+__NAME__=$(basename "$0")
 
 # 7.3. Preparing Virtual Kernel File Systems
 # https://www.linuxfromscratch.org/lfs/view/11.2/chapter07/kernfs.html
 
-echo "Mounting vkfs $LFS_BASE/{dev,proc,sys,run}"
-mkdir -pv $LFS_BASE/{dev,proc,sys,run}
+if [ "$LFS" == "" ]; then
+    echo "$__NAME__: $LFS is not defined"
+    exit 1
+fi
+
+echo "Mounting vkfs $LFS/{dev,proc,sys,run}"
+mkdir -pv $LFS/{dev,proc,sys,run}
 
 # mount and populate /dev
-mount -v --bind /dev $LFS_BASE/dev
+mount -v --bind /dev $LFS/dev
 
 # mount Virtual Kernel File Systems
-mount -v --bind /dev/pts $LFS_BASE/dev/pts
-mount -vt proc proc $LFS_BASE/proc
-mount -vt sysfs sysfs $LFS_BASE/sys
-mount -vt tmpfs tmpfs $LFS_BASE/run
+mount -v --bind /dev/pts $LFS/dev/pts
+mount -vt proc proc $LFS/proc
+mount -vt sysfs sysfs $LFS/sys
+mount -vt tmpfs tmpfs $LFS/run
 
-if [ -h $LFS_BASE/dev/shm ]; then
-    mkdir -pv $LFS_BASE/$(readlink $LFS_BASE/dev/shm)
+if [ -h $LFS/dev/shm ]; then
+    mkdir -pv $LFS/$(readlink $LFS/dev/shm)
 fi
