8915f5c96c9073c6c29a79f316e5997f38109d92
Alexander Marinov <alekmarinov@gmail.com>
2022-11-05 10:22:22 -0400

    removed syslinux

--- scripts/tools/3.1-download.sh
+++ scripts/tools/3.1-download.sh
@@ -19,13 +19,4 @@ if ! md5sum -c blfs-11.2.md5sums; then
     md5sum -c blfs-11.2.md5sums
 fi
 
-echo "Checking MD5 sum of syslinux package.."
-echo "26d3986d2bea109d5dc0e4f8c4822a459276cf021125e8c9f23c3cca5d8c850e syslinux-6.03.tar.xz" | sha256sum -c -
-if [ $? -ne 0 ]; then
-    echo "Downloading syslinux package.."
-    wget --no-check-certificate --timestamping -c --report-speed=bits https://www.kernel.org/pub/linux/utils/boot/syslinux/syslinux-6.03.tar.xz
-    echo "Checking MD5 sum of syslinux package.."
-    echo "26d3986d2bea109d5dc0e4f8c4822a459276cf021125e8c9f23c3cca5d8c850e syslinux-6.03.tar.xz" | sha256sum -c -
-fi
-
 popd
